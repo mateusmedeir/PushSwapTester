601b872e9f26f0fbb74ad398e86aa777ac96b500
matlopes <mateusmedeirtr@gmail.com>
2023-12-07 13:16:03 -0300

    refactor: enhance input tests

--- tester.sh
+++ tester.sh
@@ -12,9 +12,20 @@ RESET="\033[0m"
 OK=" ${GREEN}[ OK ]${RESET}"
 KO=" ${RED}[ KO ]${RESET}"
 
+paths=("3-basic" "5-basic")
 
 make -C ${PUSH_SWAP_PATH}
 mkdir outputs
+
+counter=0
+size_inputs=$(ls -l inputs | grep -v '^total' | wc -l)
+
+while [ $counter -lt $size_inputs ]
+do
+	mkdir outputs/${paths[$counter]}
+	counter=`expr $counter + 1`
+done
+
 clear
 
 #=================================== TESTERS ===================================#
@@ -25,62 +36,36 @@ printf "\n${BLUE}============== ${BOLD}PUSH SWAP TESTER${BLUE} ==============${R
 
 #========================= MANDATORY =========================#
 
-printf "\n${BOLD}BASIC INPUTS${RESET}\n"
+paths=("3-basic" "5-basic")
 
+counter=0
 
-
-index=0
-size=$(ls -l inputs/basic/ | wc -l)
-
-
-while [ $index -lt `expr $size - 1` ]
+while [ $counter -lt $size_inputs ]
 do
-	${PUSH_SWAP_PATH}push_swap $(cat inputs/basic/test-$index) 1> outputs/test-$index-result.txt 2> /dev/null
-	cat outputs/test-$index-result.txt | wc -l 1> outputs/test-$index-instructions.txt
-	cat outputs/test-$index-result.txt | ${PUSH_SWAP_PATH}checker $(cat inputs/basic/test-$index) 1> outputs/test-$index-check.txt
+	printf "\n${BOLD}$(echo ${paths[$counter]} | tr 'a-z' 'A-Z') INPUTS${RESET}\n"
 
-	printf "\nTest $index:"
+	index=0
+	size=$(ls -l inputs/${paths[$counter]} | grep -v '^total' | wc -l)
+	while [ $index -lt $size ]
+	do
+		${PUSH_SWAP_PATH}push_swap $(cat inputs/${paths[$counter]}/test-$index) 1> outputs/${paths[$counter]}/test-$index-result.txt 0> /dev/null
+		cat outputs/${paths[$counter]}/test-$index-result.txt | wc -l 1> outputs/${paths[$counter]}/test-$index-instructions.txt
+		cat outputs/${paths[$counter]}/test-$index-result.txt | ${PUSH_SWAP_PATH}checker $(cat inputs/${paths[$counter]}/test-$index) 1> outputs/${paths[$counter]}/test-$index-check.txt
 
-	if [ $(cat outputs/test-$index-check.txt | grep "OK" | wc -l) != 0 ]
-	then
-		printf "${OK}: ${BOLD}${YELLOW}$(cat outputs/test-$index-instructions.txt)${RESET} instructions"
-	else
-    		printf "${KO}"
-	fi
-	index=`expr $index + 1`
+		printf "\nTest $index:"
+	
+		if [ $(cat outputs/${paths[$counter]}/test-$index-check.txt | grep "OK" | wc -l) != 0 ]
+		then
+			printf "${OK}: ${BOLD}${YELLOW}$(cat outputs/${paths[$counter]}/test-$index-instructions.txt)${RESET} instructions"
+		else
+    			printf "${KO}"
+		fi
+		index=`expr $index + 1`
+	done
+
+	printf "\n"
+	counter=`expr $counter + 1`
 done
-
-
-
-printf "\n${BOLD}100 INPUTS${RESET}\n"
-
-
-
-index=0
-size=$(ls -l inputs/100 | wc -l)
-
-
-while [ $index -lt `expr $size - 1` ]
-do
-        ${PUSH_SWAP_PATH}push_swap $(cat inputs/100/test-$index) 1> outputs/test-$index-result.txt 2> /dev/null
-        cat outputs/test-$index-result.txt | wc -l 1> outputs/test-$index-instructions.txt
-        cat outputs/test-$index-result.txt | ${PUSH_SWAP_PATH}checker $(cat inputs/100/test-$index) 1> outputs/test-$index-check.txt
-
-        printf "\nTest $index:"
-
-        if [ $(cat outputs/100/test-$index-check.txt | grep "OK" | wc -l) != 0 ]
-        then
-                printf "${OK}: ${BOLD}${YELLOW}$(cat outputs/100/test-$index-instructions.txt)${RESET} instructions"
-        else
-                printf "${KO}"
-        fi
-        index=`expr $index + 1`
-done
-
-
-
-
-printf "\n"
 
 printf "\n${BLUE}==============================================${RESET}\n\n"
 printf "${OK} = passed the test\n"
